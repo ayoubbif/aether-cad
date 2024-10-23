9f587236686b8d9b573defa7a60e738ee4225b01
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-10-23 02:50:09 +0100

    Add scene configuration constants

---
+++ client/src/constants/scene-config.js
@@ -0,0 +1,15 @@
+export const SCENE_CONFIG = {
+  GRID: {
+    SIZE: 20,
+    COLOR: 0x404040,
+    DIVISIONS: 20
+  },
+  PLANE: {
+    SIZE: 10,
+    COLOR: 0xeeeeee
+  },
+  LIGHT: {
+    COLOR: 0xffffff,
+    INTENSITY: 0.5
+  }
+};
