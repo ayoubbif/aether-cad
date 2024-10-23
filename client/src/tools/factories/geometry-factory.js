918fe595fc4dc689102a613596d6ddb001672a04
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-10-23 18:50:00 +0100

    Add geometry factory

---
+++ client/src/tools/factories/geometry-factory.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+
+export class GeometryFactory {
+  static createPointGeometry(radius, segments) {
+    const geometry = new THREE.CircleGeometry(radius, segments);
+    geometry.rotateX(-Math.PI / 2);
+    return geometry;
+  }
+
+  static createPolygonGeometry(points) {
+    const shape = new THREE.Shape();
+    const firstPoint = points[0];
+
+    shape.moveTo(firstPoint.x, firstPoint.z);
+    points.slice(1).forEach(point => {
+      shape.lineTo(point.x, point.z);
+    });
+    shape.closePath();
+
+    const geometry = new THREE.ShapeGeometry(shape);
+    geometry.rotateX(Math.PI / 2);
+    return geometry;
+  }
+}
