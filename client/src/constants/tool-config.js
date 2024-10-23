9b209fe3796477ab9c6a3d6bd058c089008a733f
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-10-23 18:48:59 +0100

    Update TOOL_CONFIG constants for more precise positioning and improved styling

--- client/src/constants/tool-config.js
+++ client/src/constants/tool-config.js
@@ -1,5 +1,5 @@
 export const TOOL_CONFIG = {
-  POINT_RADIUS: 0.05,
+  POINT_RADIUS: 0.025,
   POINT_SEGMENTS: 6,
   SNAP_DISTANCE: 0.1,
   Y_OFFSET: 0.001,
@@ -7,7 +7,9 @@ export const TOOL_CONFIG = {
     LINE: 0x000000,
     POINT: 0x000000,
     HOVERED: 0xee0000,
-    POLYGON: 0x3388ff,
+    POLYGON: 0x22ccee,
     SELECTED: 0xff3366
-  }
+  },
+  POLYGON_OPACITY: 0.3,
+  SELECTED_OPACITY: 0.5
 };
