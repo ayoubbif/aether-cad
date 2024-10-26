49caf5c9d222ae3d435e77002cddb1a5bcb2a2b4
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-10-26 01:01:29 +0100

    Add logger configuration to server core

---
+++ server/core/logger.py
@@ -0,0 +1,26 @@
+﻿import logging
+import sys
+from logging.handlers import RotatingFileHandler
+
+def setup_logger(name: str) -> logging.Logger:
+  """Configure application logger."""
+  logger = logging.getLogger(name)
+  logger.setLevel(logging.INFO)
+
+  # Console handler
+  console_handler = logging.StreamHandler(sys.stdout)
+  console_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+  ))
+  logger.addHandler(console_handler)
+
+  # File handler
+  file_handler = RotatingFileHandler(
+    'app.log', maxBytes=1024*1024, backupCount=5
+  )
+  file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+  ))
+  logger.addHandler(file_handler)
+
+  return logger
